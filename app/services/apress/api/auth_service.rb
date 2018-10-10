9ebf6cae045adf8af9d146dd273c5f51b070ab10
terentev <terentev555@gmail.com>
2018-10-10 17:16:43 +0500

    test

--- app/services/apress/api/auth_service.rb
+++ app/services/apress/api/auth_service.rb
@@ -19,6 +19,16 @@ module Apress
         return false if client.secret_token_expired?
 
         return true if not_check_signature?
+        raise <<-EXCEPTION_MESSAGE
+          md5_mismatch: #{::ApiAuth.send(:md5_mismatch?, request)}
+          signatures_match: #{::ApiAuth.send(:signatures_match?, request, client.secret_token)}
+          request_too_old: #{::ApiAuth.send(:request_too_old?, request)}
+          canonical_string: #{::ApiAuth::Headers.new(request).canonical_string}
+          secret_token: #{client.secret_token}
+          access_id: #{::ApiAuth.send(:access_id, request)}
+          authorization_header: #{::ApiAuth.send(:parse_auth_header, ::ApiAuth::Headers.new(request).authorization_header)}
+          hmac_signature: #{::ApiAuth.send(:hmac_signature, request, client.secret_token)}
+        EXCEPTION_MESSAGE
         ::ApiAuth.authentic?(request, client.secret_token)
       end
 
